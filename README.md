a9e728f2eacad2775cc0d4d2356403ae7724c3af
Joonas Tikkanen <joonas.tikkanen91@gmail.com>
2023-07-03 11:07:15 +0300

    first working nixos config

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Nix Configurations
+
+Build new NixOS build:
+
+```
+$ nixos-rebuild test
+$ nixos-rebuild build -p conf-`date +'%Y%m%d%H%M'`
+$ nixos-rebuild switch -p conf-`date +'%Y%m%d%H%M'`
+```
--- joonas-linux/configuration.nix
+++ joonas-linux/configuration.nix
@@ -1,10 +1,25 @@
-{ config, pkgs, ... }:
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, lib, ... }:
 
 {
-  nixpkgs.config.allowUnfree = true;
-  i18n.defaultLocale = "en_US.UTF-8";
-  time.timeZone = "Europe/Helsinki";
-  time.hardwareClockInLocalTime = true;
+  nix.nixPath = [ "nixos-config=$PWD/configuration.nix" ];
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.supportedFilesystems = [ "ntfs" ];
+  fileSystems."/mnt/DATA" =
+    { device = "/dev/disk/by-label/DATA";
+      fsType = "ntfs-3g"; 
+      options = [ "rw" "uid=1000" "gid=1000" ];
+    };
 
   networking = {
     hostName = "joonas-linux";
@@ -12,39 +27,36 @@
     firewall.enable = true;
 
     extraHosts = ''
-      10.20.30.20 tikinas.localdomain
-      10.20.30.40 tikiproxy.localdomain
+      10.20.30.20 tikinas
+      10.20.30.40 tikiproxy
     '';
   };
 
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  boot.loader.grub.device = "nodev";
-  boot.loader.grub.useOSProber = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  # Set your time zone.
+  time.timeZone = "Europe/Helsinki";
 
-  users.users.joonas = {
-     isNormalUser = true;
-     extraGroups = [ "wheel" "kvm" "input" "disk" "libvirtd" ]; # Enable ‘sudo’ for the user.
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "fi_FI.UTF-8";
+    LC_IDENTIFICATION = "fi_FI.UTF-8";
+    LC_MEASUREMENT = "fi_FI.UTF-8";
+    LC_MONETARY = "fi_FI.UTF-8";
+    LC_NAME = "fi_FI.UTF-8";
+    LC_NUMERIC = "fi_FI.UTF-8";
+    LC_PAPER = "fi_FI.UTF-8";
+    LC_TELEPHONE = "fi_FI.UTF-8";
+    LC_TIME = "fi_FI.UTF-8";
   };
 
-  # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-
-  hardware.opengl.driSupport32Bit = true;
-  hardware.pulseaudio.support32Bit = true;
-  nixpkgs.config.allowUnfreePredicate = (pkg: builtins.elem (builtins.parseDrvName pkg.name).name [ "steam" ]);
-  nix.settings = {
-      substituters = ["https://nix-gaming.cachix.org"];
-      trusted-public-keys = ["nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="];
-    };
-
-  services.xserver = {
+  # Enable the X11 windowing system.
+ services.xserver = {
     enable = true;
     displayManager.gdm.enable = true;
     desktopManager.gnome.enable = true;
     layout = "fi";
+    xkbVariant = "";
     libinput = {
       enable = true;
 
@@ -54,31 +66,33 @@
       };
     };
 
-    deviceSection = ''
-        Identifier     "Device0"
-        Driver         "nvidia"
-        VendorName     "NVIDIA Corporation"
-        BoardName      "NVIDIA GeForce GTX 1070"
-      '';
-
-    screenSection = ''
-        Identifier     "Screen0"
-        Device         "Device0"
-        Monitor        "Monitor0"
-        DefaultDepth    24
-        Option         "Stereo" "0"
-        Option         "nvidiaXineramaInfoOrder" "DFP-6"
-        Option         "metamodes" "DP-4: 2560x1440_144 +2560+0, HDMI-0: 2560x1440_75 +0+0"
-        Option         "SLI" "Off"
-        Option         "MultiGPU" "Off"
-        Option         "BaseMosaic" "off"
-        SubSection     "Display"
-            Depth       24
-        EndSubSection
-      '';
     excludePackages = with pkgs; [
         xterm
       ];
+  };
+
+
+  # Configure console keymap
+  console.keyMap = "fi";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
   };
 
   # NVIDIA
@@ -105,7 +119,7 @@
 
     # Use the open source version of the kernel module
     # Only available on driver 515.43.04+
-    open = true;
+    open = false;
 
     # Enable the nvidia settings menu
     nvidiaSettings = true;
@@ -113,18 +127,24 @@
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
-  ## Gaming
-	programs.steam = {
-	  enable = true;
-	  remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-	  dedicatedServer.openFirewall = false; # Open ports in the firewall for Source Dedicated Server
-	};
-	  
 
-  # List services that you want to enable:
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.joonas = {
+    isNormalUser = true;
+    description = "Joonas Tikkanen";
+    extraGroups = [ "networkmanager" "wheel" "kvm" "input" "disk" "libvirtd" ];
+  };
+  
   virtualisation.libvirtd.enable = true; 
-  # enable flatpak support
-  services.flatpak.enable = true;
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
@@ -142,6 +162,12 @@
     gimp
     git
     google-chrome
+    gnomeExtensions.appindicator
+    gnomeExtensions.sound-output-device-chooser
+    gnomeExtensions.user-themes
+    gnomeExtensions.vitals
+    gnomeExtensions.blur-my-shell
+    gnomeExtensions.auto-move-windows
     gparted
     hugo
     hunspellDicts.sv_FI
@@ -176,29 +202,6 @@
           ];
         })
   ];
-  environment.gnome.excludePackages = (with pkgs; [
-    gnome-photos
-    gnome-tour
-  ]) ++ (with pkgs.gnome; [
-    cheese # webcam tool
-    gnome-music
-    epiphany # web browser
-    geary # email reader
-    evince # document viewer
-    gnome-characters
-    totem # video player
-    tali # poker game
-    iagno # go game
-    hitori # sudoku game
-    atomix # puzzle game
-  ]);
-  environment.systemPackages = with pkgs; [
-    gnomeExtensions.appindicator
-    gnomeExtensions.sound-output-device-chooser
-    gnomeExtensions.user-themes
-    gnomeExtensions.vitals
-    gnomeExtensions.blur-my-shell
-    gnomeExtensions.auto-move-windows
-   ];
-  services.udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
+  system.stateVersion = "23.05"; # Did you read the comment?
+
 }
