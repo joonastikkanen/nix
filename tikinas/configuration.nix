47ce4fbeff81ca2de8383f7aa7527d75ff652113
Joonas Tikkanen <joonas.tikkanen@ambientia.fi>
2023-11-09 20:00:21 +0200

    update working tikinas configs

--- tikinas/configuration.nix
+++ tikinas/configuration.nix
@@ -2,29 +2,34 @@
 {
   imports =
     [
-      #./hardware-configuration.nix
+      ./hardware-configuration.nix
     ];
 
-  fileSystems."/data" = {
-    device = "data";
-    fsType = "zfs";
-  };
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.supportedFilesystems = [ "zfs" ];
+  boot.zfs.extraPools = [ "data" ];
 
+  hardware.bluetooth.enable = true; # enables support for Bluetooth
+  
   networking = {
     hostName = "tikinas";
+    hostId = "8f16e7a3"; 
     networkmanager.enable = true;
     firewall = {
+      allowPing = true;
       enable = true;
-      allowedTCPPorts = [ 22 111 53 2049 3000 3306 4000 4001 4002 7575 7878 8080 8086 8096 8123 8200 8880 8920 8989 9090 20048 ];
-      allowedUDOPorts = [ 53 67 111 2049 7359 1901 4000 4001 4002 20048 ];
+      allowedTCPPorts = [ 22 111 53 2049 3000 3306 4000 4001 4002 5357 7575 7878 8080 8086 8096 8123 8200 8880 8920 8989 9090 20048 ];
+      allowedUDPPorts = [ 53 67 111 2049 7359 1901 3702 4000 4001 4002 20048 ];
     };
-    interfaces.end0.ipv4.addresses = [ {
+    interfaces.enp0s31f6.ipv4.addresses = [ {
       address = "10.20.30.20";
       prefixLength = 24;
     } ];
     defaultGateway = "10.20.30.1";
 
-    nameservers = [ "1.1.1.1" "10.20.30.20" ];
+    nameservers = [ "1.1.1.1" ];
 
     extraHosts = ''
       10.20.30.40 tikiproxy
@@ -52,7 +57,7 @@
   users.users.joonas = {
     isNormalUser = true;
     description = "Joonas Tikkanen";
-    extraGroups = [ "wheel" "docker" ];
+    extraGroups = [ "wheel" "docker" "networkmanager" ];
     openssh.authorizedKeys.keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAklY2OI4S534W3JzBM0oUhTIGTNnVIFrcypkgN4THczMm/6tnPgw9tkEPJqPrWGto2mgUhrYx18cXe+hTZLCCK+TIlXok60PosunQtZ1CIcUe30IhE927nFAUz+otN9KolyeuMKhsPK9Ei/0ZSvh4K79JwPUTi5KnZqwOnjnxlkMz/pz8pYQSGahi4H4QnRxutdchb4T8ycXrZvk1tlYVef1RnKebfvbfNb9aFx644N5+GaHGr/HwEaWLdurQMEb/h8zh1kNc7Dzfk3iGSnJvHri0AEtbr0GmI3r4Iofzgw5Ix+5hbU2+AZtNistA6+IeBG5fJgpn6On1qS7sAVgsWw== joonas.tikkanen91@gmail.com"
       "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA5VgOodGHpCrSSAPik03K3XRCGzEoDR3383h6UPsgjWQNALPGfqpJIF48Qv9SPZDIYqoBAE/kYd5v2wcMjpKVpNUnHmx8gc7fA3mvs2uSV8azrxwk5LI7vRdcKzoyg3tiy6VwJR2UMcfTMwiZD168DYJvlsPl1+Q7PUfWNfisMBpJMzY76jrB/GUn9wvGL1Ywh6Gj2Psz1hm4/gKGkthvAYnMZnBp5BYGnLjhxjWxaxQK7gBqAb3xyRWIgQJVnSJnBnbNrkOS9lzgEnhR9/jy8iBPH8ilcWuLq/cYA1aGVkHWT1tmXtMEz4+BWrUdpS5pHEOk5m9WkcE3stb6ofqADQ== joonas.tikkanen@ambientia.fi"
@@ -61,12 +66,25 @@
     ];
   };
 
+  users.users.cloudflared = {
+    group = "cloudflared";
+    isSystemUser = true;
+  };
+  users.groups.cloudflared = { };
+  systemd.services.clourflared = {
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.cloudflared}/bin/cloudflared tunnel --no-autoupdate run --token <TOKEN>";
+      Restart = "always";
+      User = "cloudflared";
+      Group = "cloudflared";
+    };
+  };
+
   services = {
     zfs = {
       autoScrub = { 
-        enable = true;
-      };
-      server = {
         enable = true;
       };
     };
@@ -86,8 +104,42 @@
     tailscale = {
       enable = true;
     };
+    samba-wsdd.enable = true;
+    samba = {
+      enable = true;
+      securityType = "user";
+      extraConfig = ''
+        workgroup = WORKGROUP
+        server string = smbnix
+        netbios name = smbnix
+        security = user 
+        #use sendfile = yes
+        #max protocol = smb2
+        # note: localhost is the ipv6 localhost ::1
+        hosts allow = 10.20.30. 127.0.0.1 localhost
+        hosts deny = 0.0.0.0/0
+        guest account = nobody
+        map to guest = bad user
+      '';
+      shares = {
+        public = {
+          path = "/data";
+          browseable = "yes";
+          "read only" = "no";
+          "guest ok" = "no";
+          "create mask" = "0644";
+          "directory mask" = "0755";
+          "force user" = "joonas";
+          "force group" = "joonas";
+        };
+      };
+    };
   };
   virtualisation.docker.enable = true;
+  virtualisation.docker.rootless = {
+    enable = true;
+    setSocketVariable = true;
+  };
   console.enable = true;
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
@@ -97,20 +149,26 @@
     curl
     docker-compose
     git
+    python311Packages.zigpy
+    python311Packages.pip
+    python311Packages.virtualenv
     htop
     jq
     neofetch
     nfs-utils
     openssl
+    python3
+    sqlite
     samba
     tmux
     unzip
     unrar
+    usbutils
     vim
     wget
     yq
   ];
 
   hardware.enableRedistributableFirmware = true;
-  system.stateVersion = "23.11";
+  system.stateVersion = "23.05";
 }
