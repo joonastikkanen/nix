ea12752f5e4e4d6519e3001d9a7dd3d5dd5ab0c9
Joonas Tikkanen <joonas.tikkanen91@gmail.com>
2024-03-13 16:07:22 +0200

    restructure and add home manager configs

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "NixOS configs for my machines";
+
+  inputs = {
+    # Nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # Home manager
+    home-manager.url = "github:nix-community/home-manager/";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    # NixOS profiles to optimize settings for different hardware
+    hardware.url = "github:nixos/nixos-hardware";
+
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+
+    # Function for NixOS system configuration
+    nixosSystemFor = hostname: configurationFile:
+      nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
+        modules = [configurationFile];
+      };
+
+    # Function for Home Manager configuration
+    homeManagerFor = user: hostname: {
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      extraSpecialArgs = {inherit inputs outputs;};
+      modules = [./home/${user}/${hostname}.nix];
+    };
+  in {
+    nixosConfigurations = {
+      joonas-linux = nixosSystemFor "joonas-linux" ./hostnames/joonas-linux/configuration.nix;
+      matebook = nixosSystemFor "matebook" ./hostnames/matebook/configuration.nix;
+    };
+
+    homeConfigurations = {
+      "joonas@joonas-linux" = home-manager.lib.homeManagerConfiguration (homeManagerFor "jooas" "joonas-linux");
+      "joonas@matebook" = home-manager.lib.homeManagerConfiguration (homeManagerFor "joonas" "matebook");
+    };
+  };
+}
