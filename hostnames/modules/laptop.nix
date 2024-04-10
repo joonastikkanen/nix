a0d56fbaaa046f430eb02c77648a75411d4e467f
Joonas Tikkanen <joonas.tikkanen91@gmail.com>
2024-04-10 12:26:33 +0300

    update charge percent on laptop module

--- hostnames/modules/laptop.nix
+++ hostnames/modules/laptop.nix
@@ -22,7 +22,7 @@
 
        #Optional helps save long term battery health
        START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
-       STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+       STOP_CHARGE_THRESH_BAT0 = 95; # 80 and above it stops charging
 
       };
     };
