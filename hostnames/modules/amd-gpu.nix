4a96141f9096568e825b6cbf5cc6e5bed3fa9a21
Joonas Tikkanen <joonas.tikkanen91@gmail.com>
2024-09-22 20:00:29 +0300

    update amd and sound nix modules

--- hostnames/modules/amd-gpu.nix
+++ hostnames/modules/amd-gpu.nix
@@ -2,14 +2,12 @@
 {
     services.xserver.enable = true;
     services.xserver.videoDrivers = [ "amdgpu" ];
-    hardware.opengl.driSupport = true; # This is already enabled by default
-    hardware.opengl.driSupport32Bit = true; # For 32 bit applications
 
-    hardware.opengl.extraPackages = with pkgs; [
+    hardware.graphics.extraPackages = with pkgs; [
         amdvlk
     ];
     # For 32 bit applications 
-    hardware.opengl.extraPackages32 = with pkgs; [
+    hardware.graphics.extraPackages32 = with pkgs; [
         driversi686Linux.amdvlk
     ];
 }
