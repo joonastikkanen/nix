a502f3020f8bffb9a4c0d969900d73ff98963a6d
Joonas Tikkanen <joonas.tikkanen91@gmail.com>
2024-11-03 22:26:13 +0200

    update flake and add trusteed users

--- hostnames/modules/common.nix
+++ hostnames/modules/common.nix
@@ -32,12 +32,8 @@
     nix.settings = {
         experimental-features = "nix-command flakes";
         auto-optimise-store = true;
-    };
-    nix.extraOptions = ''
-        trusted-users = root joonas
-    '';
-    nix.extraOptions = ''
+        trusted-users = ["root joonas" "@wheel"];
         extra-substituters = https://devenv.cachix.org;
         extra-trusted-public-keys = devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=;
-    '';
+    };
 }
